6450d77dab2f82ec9380a766b8c2fa8decea7024
Žiga <ziga_strgar@hotmail.com>
2015-01-30 11:57:57 +0100

    Date and time picker

---
+++ markup.php
@@ -0,0 +1,4 @@
+<div class="input-group date datetime" data-min-view="2" data-start-view="4" lang="sl" data-date="<?php echo $today; ?>" data-date-format="dd-mm-yyyy" >
+    <span class="input-group-addon btn btn-primary"><span class="glyphicon glyphicon-th"></span></span>
+    <input class="form-control" name="start"  maxlength="12" size="16" type="text">
+</div>
