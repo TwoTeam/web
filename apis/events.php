c22683f4d0f998c51ae64b8ac07018003354ff90
Žiga <ziga_strgar@hotmail.com>
2015-02-04 08:31:42 +0100

    Fetch events API

--- apis/events.php
+++ apis/events.php
@@ -1,2 +1,17 @@
 <?php
 include_once '../database.php';
+
+$user = $_POST["user_id"];
+
+$query = "SELECT * FROM events WHERE deleted = 0";
+$result = mysqli_query($link, $query);
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_array($result)) {
+        $response[] = $row;
+    }
+} else {
+    $response = array("message" => "Trenutno ni dodanih dogodkov!");
+}
+
+echo json_encode($response);
