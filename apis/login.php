c48a65dea5356b6ffd6fa5329a509b251462275e
Žiga <ziga_strgar@hotmail.com>
2015-02-05 08:01:52 +0100

    Login change

--- apis/login.php
+++ apis/login.php
@@ -1,9 +1,9 @@
 <?php
 include_once '../database.php';
-$username = $_POST["username"];
+$email = $_POST["email"];
 $password = sha1($_POST["password"]);
 
-$query = sprintf("SELECT * FROM users WHERE username = '%s' AND password = '%s'", mysqli_real_escape_string($link, $username), mysqli_real_escape_string($link, $password));
+$query = sprintf("SELECT * FROM users WHERE email = '%s' AND password = '%s'", mysqli_real_escape_string($link, $email), mysqli_real_escape_string($link, $password));
 $result = mysqli_query($link, $query);
 
 if(mysqli_num_rows($result) == 1){
